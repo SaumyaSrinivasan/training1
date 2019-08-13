b6c74be64d56bc74bb6f25ce5c31dd706cf8b5d8
SaumyaSrinivasan <52213259+SaumyaSrinivasan@users.noreply.github.com>
2019-08-13 19:03:37 +0530

    Update vowrcons.cpp

--- vowrcons.cpp
+++ vowrcons.cpp
@@ -9,7 +9,7 @@ int main() {
         
            if((c== 'a'||c== 'e'||c== 'i'||c== 'o'||c== 'u'))
            {
-               cout<<"Vowels";
+               cout<<"Vowel";
            }
            else
            {
