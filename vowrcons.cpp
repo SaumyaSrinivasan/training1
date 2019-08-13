dbad6fac92e423ef2411f504a44da76bef17767a
SaumyaSrinivasan <52213259+SaumyaSrinivasan@users.noreply.github.com>
2019-08-13 19:02:12 +0530

    Create vowrcons.cpp

---
+++ vowrcons.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cin>>c;
+    if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        
+           if((c== 'a'||c== 'e'||c== 'i'||c== 'o'||c== 'u'))
+           {
+               cout<<"Vowels";
+           }
+           else
+           {
+               cout<<"Consonant";
+           }
+    }
+    else
+    {
+        cout<<"invalid";
+    }
+	return 0;
+}
