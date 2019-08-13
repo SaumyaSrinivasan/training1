2a6f7f305ce2c98b751973ec8b7ba054b88f0181
SaumyaSrinivasan <52213259+SaumyaSrinivasan@users.noreply.github.com>
2019-08-13 18:24:47 +0530

    Create posrneg.java

---
+++ posrneg.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Program
+
+{
+	public static void main(String[] args) {
+	    int n;
+	    Scanner in=new Scanner(System.in);
+	    n=in.nextInt();
+	    if(n > 0)
+	    {
+	        System.out.println("Positive");
+	    }
+	    else if(n==0)
+	    {
+	        System.out.println("Zero");
+	        
+	    }
+	    else
+	    {
+	        System.out.println("Negative");
+	    }
+	    
+	    
+		
+	}
+}
