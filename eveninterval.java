efe40a0db69ca7f08b973c355580594d59890ed9
SaumyaSrinivasan <52213259+SaumyaSrinivasan@users.noreply.github.com>
2019-08-18 21:17:58 +0530

    Create eveninterval.java

---
+++ eveninterval.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Program
+{
+	public static void main(String[] args) {
+		
+		int start,end;
+		Scanner in=new Scanner(System.in);
+		start=in.nextInt();
+		end=in.nextInt();
+		for(int i=start+1;i<end;i++)
+		{
+		    if(i%2==0)
+		    {
+		        System.out.print(i+" ");
+		    }
+		}
+		
+	}
+}
